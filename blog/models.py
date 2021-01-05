23e17dede65c88de9385259431dda53c2737230e
KangInSun <s20002@gsm.hs.kr>
2021-01-05 10:46:39 +0900

    category in post_list

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -9,6 +16,8 @@ class Post(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
--- blog/tests.py
+++ blog/tests.py
@@ -1,30 +1,66 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-def create_post(title, content, author):
+def create_category(name='result', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+
+    return category
+
+def create_post(title, content, author, category=None):
     blog_post = Post.objects.create(
         title=title,
         content=content,
         created=timezone.now(),
-        author=author
+        author=author,
+        category=category,
     )
 
     return blog_post
 
+class TestModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='sanha', password='nopassword')
+
+    def test_category(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world.',
+            author=self.author_000,
+            category=category
+        )
+
+        self.assertEqual(category.post_set.count(), 1)
+
+    def test_post(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world.',
+            author=self.author_000,
+            category=category
+        )
+
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
-        self.author_000 = User.objects.create(username='insun',password='nopassword')
+        self.author_000 = User.objects.create(username='sanha',password='nopassword')
 
     def check_navbar(self, soup):
         navbar = soup.find('div', id='navbar')
         self.assertIn('오늘 뭐 볼까? 자유게시판', navbar.text)
         self.assertIn('About me', navbar.text)
 
-    def test_post_list(self):
+    def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
@@ -38,10 +74,19 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 0)
         self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
+    def test_post_list_with_post(self):
+
         post_000 = create_post(
             title='The first post',
             content='Hello World. We are the world.',
-            author=self.author_000
+            author=self.author_000,
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='결과 공유')
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -55,6 +100,16 @@ class TestView(TestCase):
 
         post_000_read_more_btn = body.find('a', id='read-more-post-{}'.format(post_000.pk))
         self.assertEqual(post_000_read_more_btn['href'], post_000.get_absolute_url())
+
+        # category card 에서
+        category_card = body.find('div', id='category-card')
+        self.assertIn('미분류 (1)', category_card.text)  # 미분류 (1) 있어야 함
+        self.assertIn('결과 공유 (1)', category_card.text)  # 결과 공유 (1) 있어야 함
+
+        # main_div에는
+        main_div = body.find('div', id='main_div')
+        self.assertIn('결과 공유', main_div.text)  # '결과 공유' 있어야 함
+        self.assertIn('미분류', main_div.text)  # '미분류' 있어야 함
 
     def test_post_detail(self):
         post_000 = create_post(
