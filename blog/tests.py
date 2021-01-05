d4ce64d3d3f7ff43cbd946fc68a3f9080bd39db9
KangInSun <s20002@gsm.hs.kr>
2021-01-05 11:02:26 +0900

    categories in post_detail page

--- blog/tests.py
+++ blog/tests.py
@@ -60,6 +60,11 @@ class TestView(TestCase):
         self.assertIn('오늘 뭐 볼까? 자유게시판', navbar.text)
         self.assertIn('About me', navbar.text)
 
+    def check_right_side(self, soup):
+        category_card = soup.find('div', id='category-card')
+        self.assertIn('미분류 (1)', category_card.text)  # 미분류 (1) 있어야 함
+        self.assertIn('결과 공유 (1)', category_card.text)  # 결과 공유 (1) 있어야 함
+
     def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
@@ -101,13 +106,10 @@ class TestView(TestCase):
         post_000_read_more_btn = body.find('a', id='read-more-post-{}'.format(post_000.pk))
         self.assertEqual(post_000_read_more_btn['href'], post_000.get_absolute_url())
 
-        # category card 에서
-        category_card = body.find('div', id='category-card')
-        self.assertIn('미분류 (1)', category_card.text)  # 미분류 (1) 있어야 함
-        self.assertIn('결과 공유 (1)', category_card.text)  # 결과 공유 (1) 있어야 함
+        self.check_right_side(soup)
 
         # main_div에는
-        main_div = body.find('div', id='main_div')
+        main_div = soup.find('div', id='main_div')
         self.assertIn('결과 공유', main_div.text)  # '결과 공유' 있어야 함
         self.assertIn('미분류', main_div.text)  # '미분류' 있어야 함
 
@@ -116,6 +118,13 @@ class TestView(TestCase):
             title='The first post',
             content='Hello World. We are the world.',
             author=self.author_000
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='결과 공유')
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -140,3 +149,5 @@ class TestView(TestCase):
         self.assertIn(post_000.author.username, main_div.text)
 
         self.assertIn(post_000.content, main_div.text)
+
+        self.check_right_side(soup)
