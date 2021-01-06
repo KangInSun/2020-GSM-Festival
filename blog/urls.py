b162cb411824c6419ff27bc3d060d54a30cb64b7
KangInSun <s20002@gsm.hs.kr>
2021-01-06 21:26:44 +0900

    connected : main_page

--- blog/urls.py
+++ blog/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('<int:pk>/update/', views.PostUpdate.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('create/', views.PostCreate.as_view()),
-    path('', views.PostList.as_view()),
+    path('blog/', views.PostList.as_view()),
+    path("main", views.main_view)
 ]
--- gsm_festival_prj/urls.py
+++ gsm_festival_prj/urls.py
@@ -18,11 +18,13 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+import blog.views
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
+    path('main/', blog.views.main_view)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
